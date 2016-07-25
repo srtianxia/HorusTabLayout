ef8335a065d8cc8cbc1d57b9f1b2657535c20acd
srtianxia <srtianxia@163.com>
2016-07-25 20:42:38 +0800

    commit

--- app/src/main/java/com/srtianxia/horustablayout/MainActivity.java
+++ app/src/main/java/com/srtianxia/horustablayout/MainActivity.java
@@ -2,12 +2,27 @@ package com.srtianxia.horustablayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import com.srtianxia.tablayoutlibs.HorusTabLayout;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private HorusTabLayout mHorusTabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mHorusTabLayout = (HorusTabLayout) findViewById(R.id.tab_layout);
+        List<String> titles = new ArrayList<>();
+        titles.add("标题1");
+        titles.add("标题2");
+        titles.add("标题3");
+        mHorusTabLayout.setTitle(titles);
+        mHorusTabLayout.setOnTabSelectListener(new HorusTabLayout.OnTabSelectListener() {
+            @Override public void onClick(int position) {
+                Log.d("position --------->" , String.valueOf(position));
+            }
+        });
     }
 }
--- tablayoutlibs/src/main/java/com/srtianxia/tablayoutlibs/HorusTabLayout.java
+++ tablayoutlibs/src/main/java/com/srtianxia/tablayoutlibs/HorusTabLayout.java
@@ -1,19 +1,59 @@
 package com.srtianxia.tablayoutlibs;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by srtianxia on 2016/7/25.
  */
 public class HorusTabLayout extends View {
     private int mScreenWidth;
+    private int mItemWidth;
+    private int mItemSize;
+    private List<String> mItemsTitle = new ArrayList<>();
+
+    private Paint mTextPaint;
+    private Paint mIndicatorPaint;
+    private Path mPath;
+
+    private String mColorChoose = "#ffffff";
+    private String mColorUnChoose = "#000000";
+
+    private OnTabSelectListener mOnTabSelectListener;
+
+    private int mPosition = 0;
+
+    private int mLastPosition = 0;
+
+    private float mPositionOffset = 0;
+
+    private boolean isInit = false;
+
+
+    public void setOnTabSelectListener(OnTabSelectListener mOnTabSelectListener) {
+        this.mOnTabSelectListener = mOnTabSelectListener;
+    }
 
 
     public HorusTabLayout(Context context) {
         this(context, null);
+    }
+
+
+    public void setTitle(List<String> itemsTitle) {
+        this.mItemsTitle = itemsTitle;
+        mItemWidth = mScreenWidth / itemsTitle.size();
     }
 
 
@@ -25,11 +65,33 @@ public class HorusTabLayout extends View {
     public HorusTabLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mScreenWidth = getResources().getDisplayMetrics().widthPixels;
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.HorusTabLayout);
+        typedArray.recycle();
+
+        mTextPaint = new Paint();
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setTextSize(50);
+        mIndicatorPaint = new Paint();
+        mIndicatorPaint.setAntiAlias(true);
+        mPath = new Path();
     }
 
 
     @Override protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        int i = 0;
+        for (String title : mItemsTitle) {
+            if (i == mPosition) {
+                drawText(canvas, i, mColorChoose, title);
+                drawIndicator(canvas, i, mColorChoose);
+            } else {
+                drawText(canvas, i, mColorUnChoose, title);
+                //drawIndicator(canvas, i, mColorUnChoose);
+            }
+            i++;
+        }
     }
 
 
@@ -40,5 +102,73 @@ public class HorusTabLayout extends View {
 
     @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+
+    private void drawText(Canvas canvas, int index, String color, String title) {
+        mTextPaint.setColor(Color.parseColor(color));
+        canvas.drawText(title,
+            mItemWidth * index + mItemWidth / 2f - mTextPaint.measureText(title) / 2f,
+            getHeight() / 2f - (mTextPaint.descent() + mTextPaint.ascent()) / 2f,
+            mTextPaint);
+    }
+
+
+    private void drawIndicator(Canvas canvas, int index, String color) {
+        mIndicatorPaint.setColor(Color.parseColor(color));
+        mPath.reset();
+        mPath.moveTo(mPositionOffset * mItemWidth + mItemWidth / 2,
+            getHeight() - 8);
+        mPath.lineTo(mPositionOffset * mItemWidth + mItemWidth / 2 + 8,
+            getHeight());
+        mPath.lineTo(mPositionOffset * mItemWidth + mItemWidth / 2 - 8,
+            getHeight());
+        canvas.drawPath(mPath, mIndicatorPaint);
+    }
+
+
+    //获取Text高度
+    private int getTextHeight(Paint paint, String str) {
+        Rect rect = new Rect();
+        paint.getTextBounds(str, 0, str.length(), rect);
+        return (int) (rect.height() / 33f * 29);
+    }
+
+
+    @Override public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mLastPosition = mPosition;
+                mPosition = (int) Math.floor(event.getX() / mItemWidth);
+                if (mLastPosition != mPosition) {
+                    startMoveAnim();
+                }
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                if (mOnTabSelectListener != null) {
+                    mOnTabSelectListener.onClick(mPosition);
+                }
+                break;
+        }
+        return true;
+    }
+
+
+    private void startMoveAnim() {
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(mLastPosition, mPosition);
+        valueAnimator.setDuration(100);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                mPositionOffset = (float) valueAnimator.getAnimatedValue();
+                invalidate();
+            }
+        });
+        valueAnimator.start();
+    }
+
+
+    public interface OnTabSelectListener {
+        void onClick(int position);
     }
 }
